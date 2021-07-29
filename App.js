d3c858ac2e1a771c6bc11933675b990fac8bae1b
Zubiik <elodiefrappat08@gmail.com>
2021-07-29 10:52:56 -0400

    fix: src bug

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Handler from './Handler';
+import Handler from './src/Handler';
 
 export default function App() {
   return (
--- src/Handler/index.js
+++ src/Handler/index.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MyTopBar from '../../MyTopBar/Index';
 import MyBottomBar from '../MyBottomBar';
 import LinkPage from '../LinkPage';
+
 const Handler = () => {
 const [searchInput, setSearchInput] = useState("");
 const [linkInput, setLinkInput] = useState("");
@@ -12,8 +12,8 @@ const [titleInput, setTitleInput] = useState("");
 
   return (
     <View >
-      <MyTopBar searchInput={searchInput} setSearchInput={setSearchInput}/>
-      <MyBottomBar/>
+      {/*<MyTopBar searchInput={searchInput} setSearchInput={setSearchInput}/>
+      <MyBottomBar/>*/}
       <LinkPage 
       linkInput={linkInput}
       setLinkInput={setLinkInput}
