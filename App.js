ef60bda449769b38948645c18e185ed94b00a243
Zubiik <elodiefrappat08@gmail.com>
2021-08-04 08:40:22 -0400

    feat: router

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Handler from './src/Handler';
 
+const Stack = createStackNavigator();
+
 export default function App() {
+  
+
   return (
-    <View >
-    <Handler/>
-    </View>
+    <NavigationContainer>
+  <Stack.Navigator>
+  <Stack.Screen 
+        name="Home" 
+        component={Handler} 
+        options={{ title: 'Home' }}
+      />
+  </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
