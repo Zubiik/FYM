709b611407ab7c4033452563486c50f7a42c1db9
Zubiik <elodiefrappat08@gmail.com>
2021-08-04 08:57:18 -0400

    bug: add styled but dont work

---
+++ src/MyTopBar/styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/react';
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 50px;
+  background-color: blue;
+`;
+
+const CustomFlatList = styled.FlatList`
+display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 50px;
+  color: blue;
+`;
