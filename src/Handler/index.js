c80771f1b73ba2fd10da9a9aabd4cba23b9e0c68
Zubiik <elodiefrappat08@gmail.com>
2021-08-11 07:39:53 -0400

    bug: import doesnt work

--- src/Handler/index.js
+++ src/Handler/index.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LinkPage from '../LinkPage';
+import LinkPage from '../LinkPage/index';
 import MapList from '../MapList';
+//import MyTopBar from '../MyTopBar';
+//import MyBottomBar from '../myBottomBar';
 
 const Handler = () => {
 const [searchInput, setSearchInput] = useState("");
@@ -13,14 +15,20 @@ const [titleInput, setTitleInput] = useState("");
   return (
     <View >
       <MapList/>
-      {/*<MyTopBar searchInput={searchInput} setSearchInput={setSearchInput}/>
-      <MyBottomBar/>*/}
+      
+      {/*<MyBottomBar/>
+      <MyTopBar 
+      searchInput={searchInput} 
+      setSearchInput={setSearchInput}
+      />
+      <MyBottomBar/>
       <LinkPage 
       linkInput={linkInput}
       setLinkInput={setLinkInput}
       titleInput={titleInput}
       setTitleInput={setTitleInput}
       />
+      */}
     </View>
 
   );
