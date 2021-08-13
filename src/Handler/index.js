759819aaf8b04356752b1603a45eb0c3f6d08ba8
Zubiik <elodiefrappat08@gmail.com>
2021-08-13 09:38:23 -0400

    change import

--- src/Handler/index.js
+++ src/Handler/index.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LinkPage from '../LinkPage/index';
+//import LinkPage from '../LinkPage/index';
 //import MapList from '../MapList';
+import MyBottomBar from '../MyBottomBar';
 //import MyTopBar from '../MyTopBar';
-//import MyBottomBar from '../myBottomBar';
 
 const Handler = () => {
 const [searchInput, setSearchInput] = useState("");
@@ -14,20 +14,20 @@ const [titleInput, setTitleInput] = useState("");
 
   return (
     <View >
-      
+      <MyBottomBar/>
+     
+      {/*
+      <MapList/>
       <LinkPage 
       linkInput={linkInput}
       setLinkInput={setLinkInput}
       titleInput={titleInput}
       setTitleInput={setTitleInput}
       />
-      {/*<MyBottomBar/>
       <MyTopBar 
       searchInput={searchInput} 
       setSearchInput={setSearchInput}
       />
-      <MyBottomBar/>
-      <MapList/>
       */}
     </View>
 
